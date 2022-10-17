7047d62e9172617f414ef16cacba633011b09dbe
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-10-17 16:47:30 +0530

    Update basic_sql_select.sql

--- basic_sql_select.sql
+++ basic_sql_select.sql
@@ -150,4 +150,66 @@ where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 **Solution**
 '''
-SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;       
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION; 
+
+'''
+/**[Weather Observation Station 5](https://www.hackerrank.com/challenges/weather-observation-station-5)**/
+
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+
+Input Format
+
+The STATION table is described as follows:
+
+|  Field | Type |
+|---|---|
+| ID  | NUMBER |
+| CITY | VARCHAR2(21)   |
+| STATE  | VARCHAR2(2)  |
+| LAT_N |  NUMBER |
+| LONG_W | NUMBER |
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+*Sample Input*
+
+Lets say that CITY only has four entries: DEF, ABC, PQRS and WXY
+
+*Sample Output*
+
+ABC 3 
+
+PQRS 4
+
+*Explanation*
+
+When ordered alphabetically, the CITY names are listed as ABC, DEF, PQRS, and WXY, with the respective lengths 3,3,4,3,3,4, and 33. The longest-named city is obviously PQRS, but there are 33 options for shortest-named city; we choose ABC, because it comes first alphabetically.
+'''
+select city, length(city) from station order by length(city) DESC,city ASC fetch first row only;
+select city, length(city) from station order by length(city) asc ,city asc fetch first row only;      
+
+'''
+
+###**[Weather Observation Station 6](https://www.hackerrank.com/challenges/weather-observation-station-6)**
+
+Query the list of CITY names starting with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+|  Field | Type |
+|---|---|
+| ID  | NUMBER |
+| CITY | VARCHAR2(21)   |
+| STATE  | VARCHAR2(2)  |
+| LAT_N |  NUMBER |
+| LONG_W | NUMBER |
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+**Solution**
+'''
+SELECT DISTINCT(CITY) FROM STATION WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' 
+OR CITY LIKE 'U%' ORDER BY CITY ASC;       
+
