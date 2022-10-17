3d70435d2ba999d7175a40651afc8e98ab429bb2
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-10-17 16:53:48 +0530

    Update basic_sql_select.sql

--- basic_sql_select.sql
+++ basic_sql_select.sql
@@ -235,4 +235,13 @@ SELECT DISTINCT CITY FROM STATION WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CIT
 Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
 '''
 SELECT DISTINCT CITY FROM STATION WHERE upper(SUBSTR(CITY,1,1)) NOT IN ('A','E','I','O','U') AND lower(SUBSTR(CITY,1,1)) NOT IN
-('a','e','i','o','u');     
+('a','e','i','o','u'); 
+
+
+'''
+###**[Weather Observation Station 10](https://www.hackerrank.com/challenges/weather-observation-station-10/problem)**
+
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+'''
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('A','E','I','O','U') AND LOWER(SUBSTR(CITY, LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');    
+
