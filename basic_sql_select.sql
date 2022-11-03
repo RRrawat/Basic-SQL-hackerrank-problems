5a78040340be06c5fed349cf8c5286ccd34c5707
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-11-03 21:22:20 +0530

    Update basic_sql_select.sql

--- basic_sql_select.sql
+++ basic_sql_select.sql
@@ -5,9 +5,9 @@ Input Format
 '''
 SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
 
+'''
 
 /*[Revising the Select Query-2](https://www.hackerrank.com/challenges/revising-the-select-query-2)**/
-'''
 Query the names of all American cities in CITY with populations larger than 120,000. The CountryCode for 
 America is USA.
 Input Format
